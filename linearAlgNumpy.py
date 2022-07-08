4d57e7641daa274a4f85876af7f5f140b3120808
Rahul Shah <68120349+rahulnshah@users.noreply.github.com>
2022-07-08 15:56:16 -0400

    Update linearAlgNumpy.py

--- linearAlgNumpy.py
+++ linearAlgNumpy.py
@@ -79,32 +79,14 @@ t_of_u = np.matmul(sq_mat, u)
 print("image under T of u:\n", t_of_u) 
 
 
-'''Let T: R^2 -> R^2 be a linear transformation that maps
-u D
-#
-5
-2
-$
-into #
-2
-1
-$
-and maps v D
-#
-1
-3
-$
-into #
-"1
-3
-$
-. Use the
+'''Let T: R^2 -> R^2 be a linear transformation that maps vector
+u into [2, 1] and maps vector v into [1,3] Use the
 fact that T is linear to find the images under T of 3u, 2v, and
-3u C 2v.'''
+3u + 2v.'''
 
 '''Explanation:
-    T(3u + 2v).  T(cu) = cT(u) T(u + v) = T(u) + T(v)
-    = T(3u) + T(2v) = 3T(u) + 2T(v)'''
+    T(cu) = cT(u) and T(u + v) = T(u) + T(v)
+    Therefore, T(3u + 2v) = T(3u) + T(2v) = 3T(u) + 2T(v)'''
 t_of_u = np.array([2,1])
 t_of_v = np.array([-1,3])
 t_of_3u_p_2v = np.add(np.multiply(t_of_u, 3),np.multiply(t_of_v, 2))
